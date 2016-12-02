efbc8e1b83dc8d32440e88cd1d54ed298bb70488
Jonathan Tamsut <jtamsut@users.noreply.github.com>
2016-12-02 08:59:31 -0800

    add login/signup form (#2)
    * add login/signup form
    
    * remove npm-debug.log

--- client/login/components/Header.js
+++ client/login/components/Header.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
-const Header = ({ children }) => (
+const Header = ({
+  children,
+  username,
+  password,
+  onChangeUsername,
+  onChangePassword }) => (
     <div>
-        This is my login head
+      <FormGroup>
+        <ControlLabel>
+          Username:
+        </ControlLabel>
+        <FormControl
+          type="text"
+          placeholder="Enter username"
+          value={username}
+          onChange={onChangeUsername}
+        />
+      </FormGroup>
+      <FormGroup>
+        <ControlLabel>
+          Password:
+        </ControlLabel>
+        <FormControl
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={onChangePassword}
+        />
+      </FormGroup>
+        This is my login
         {children}
     </div>
 );
